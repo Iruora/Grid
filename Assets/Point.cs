040876ded21f329a4888b99e5a30986e0284450e
nidhal.aroui <nidhal.aroui@allence-tunisie.com>
2018-07-18 08:54:30 +0100

    /dev/gridMaking

---
+++ Assets/Point.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour {
+    public bool orsomInstance;
+    private Vector3 pz;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+    private void Awake()
+    {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pz.z = 0;
+
+        if (orsomInstance)
+        orsom.Instance.Orsomli5at(transform.position, pz);
+	}
+    private void OnMouseDown()
+    {
+        print("Clickeni !");
+        orsomInstance = true;
+        orsom.Instance.mouseDown = true;
+    }
+    
+    private void OnMouseUp()
+    {
+        print("Sayabni !");
+        orsomInstance = false;
+        orsom.Instance.mouseDown = false;
+        orsom.Instance.Orsomli5at(transform.position, pz);
+
+    }
+}
---
+++ Assets/orsom.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orsom : MonoBehaviour
+{
+    public int gridHeight;
+    public int gridWidth;
+    public Vector3 startPosition;
+    public bool mouseDown;
+    public bool isDrawing;
+    public bool lineRendererInited;
+    public GameObject lineRendererPF;
+    public int[] grid;
+    public GameObject pointPf;
+    // Use this for initialization
+    private static orsom _instance;
+
+    public static orsom Instance { get { return _instance; } }
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    void Start()
+    {
+        grid = new int[gridHeight * gridWidth];
+        for (int i = 0; i < gridHeight; i++)
+        {
+            for (int j = 0; j < gridWidth; j++)
+            {
+                GameObject obj = Instantiate(pointPf, startPosition + new Vector3(i, j, 0), Quaternion.identity);
+                if (i >= (int)gridWidth / 2)
+                {
+                    obj.AddComponent<CircleCollider2D>().radius = 0.2f;
+                }
+            }
+        }
+    }
+    public void Orsomli5at(Vector3 posA, Vector3 posB)
+    {
+
+        if (mouseDown)
+        {
+
+            if (!isDrawing)
+            {
+                GameObject lrPF = Instantiate(lineRendererPF);
+
+                LineRenderer lineRenderer = lrPF.GetComponent<LineRenderer>();
+                lineRenderer.SetPosition(0, posA);
+                isDrawing = true;
+                if (isDrawing)
+                {
+                    print(" drawing");
+                    lineRenderer.SetPosition(1, posB);
+                }
+
+            }
+
+
+        }
+        if (!mouseDown)
+        {
+            isDrawing = false;
+        }
+
+
+
+    }
+}
